dfe3777bd1acc3406584f98e71262a4a31a7f772
Made Suryawan <suryawankusuma@gmail.com>
2025-08-18 17:48:30 +0000

    on mobile device, move theme switcher in nav bar, on the left side of th

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,10 +9,10 @@ import { ThemeSwitcher } from "./ThemeSwitcher";
 import {
     Sheet,
     SheetContent,
-    SheetDescription,
     SheetHeader,
     SheetTitle,
     SheetTrigger,
+    SheetDescription
 } from "@/components/ui/sheet";
 import LogoIcon from "./LogoIcon";
 
@@ -29,126 +29,125 @@ export default function Header(): JSX.Element {
                 <LogoIcon className="mt-2 md:mt-5 h-full w-[96px] md:w-[110px] text-primary" />
                 <span className="sr-only">BaliBlissed</span>
             </Link>
-            <nav className="ml-auto hidden md:flex gap-4 sm:gap-6 items-center">
-                <Link
-                    href="/"
-                    className="text-md font-medium hover:text-primary transition-colors hover:underline underline-offset-4"
-                    prefetch={false}
-                >
-                    Home
-                </Link>
-                <Link
-                    href="/private-car-charter"
-                    className="text-md font-medium hover:text-primary transition-colors hover:underline underline-offset-4"
-                    prefetch={false}
-                >
-                    Car Charter
-                </Link>
-                <Link
-                    href="/#destinations"
-                    className="text-md font-medium hover:text-primary transition-colors hover:underline underline-offset-4"
-                    prefetch={false}
-                >
-                    Destinations
-                </Link>
-                <Link
-                    href="/#packages"
-                    className="text-md font-medium hover:text-primary transition-colors hover:underline underline-offset-4"
-                    prefetch={false}
-                >
-                    Packages
-                </Link>
-                <Link
-                    href="/#contact"
-                    className="text-md font-medium hover:text-primary transition-colors hover:underline underline-offset-4"
-                    prefetch={false}
-                >
-                    Contact
-                </Link>
-                <Button
-                    asChild
-                    className="hidden sm:inline-flex bg-accent text-accent-foreground hover:bg-accent/90"
-                >
-                    <Link href="/#contact" prefetch={false}>
-                        Book Now
-                    </Link>
-                </Button>
-                <ThemeSwitcher />
-            </nav>
-            <Sheet open={isMenuOpen} onOpenChange={setIsMenuOpen}>
-                <SheetTrigger asChild>
-                    <Button
-                        variant="outline"
-                        size="icon"
-                        className="ml-auto md:hidden"
+            <div className="ml-auto flex items-center gap-4">
+                <nav className="hidden md:flex gap-4 sm:gap-6 items-center">
+                    <Link
+                        href="/"
+                        className="text-md font-medium hover:text-primary transition-colors hover:underline underline-offset-4"
+                        prefetch={false}
                     >
-                        <Menu className="h-6 w-6" />
-                        <span className="sr-only">Toggle navigation menu</span>
+                        Home
+                    </Link>
+                    <Link
+                        href="/private-car-charter"
+                        className="text-md font-medium hover:text-primary transition-colors hover:underline underline-offset-4"
+                        prefetch={false}
+                    >
+                        Car Charter
+                    </Link>
+                    <Link
+                        href="/#destinations"
+                        className="text-md font-medium hover:text-primary transition-colors hover:underline underline-offset-4"
+                        prefetch={false}
+                    >
+                        Destinations
+                    </Link>
+                    <Link
+                        href="/#packages"
+                        className="text-md font-medium hover:text-primary transition-colors hover:underline underline-offset-4"
+                        prefetch={false}
+                    >
+                        Packages
+                    </Link>
+                    <Link
+                        href="/#contact"
+                        className="text-md font-medium hover:text-primary transition-colors hover:underline underline-offset-4"
+                        prefetch={false}
+                    >
+                        Contact
+                    </Link>
+                    <Button
+                        asChild
+                        className="hidden sm:inline-flex bg-accent text-accent-foreground hover:bg-accent/90"
+                    >
+                        <Link href="/#contact" prefetch={false}>
+                            Book Now
+                        </Link>
                     </Button>
-                </SheetTrigger>
-                <SheetContent side="right">
-                    <SheetHeader className="sr-only">
-                        <SheetTitle>Mobile Menu</SheetTitle>
-                        <SheetDescription>
-                            Navigation links for the BaliBlissed website.
-                        </SheetDescription>
-                    </SheetHeader>
-                    <nav className="grid max-w-4 gap-8 text-lg font-medium mt-4">
-                        <Link
-                            href="/"
-                            className="flex items-center gap-2 text-lg font-semibold"
-                            prefetch={false}
-                            onClick={() => setIsMenuOpen(false)}
+                </nav>
+                <ThemeSwitcher />
+                <Sheet open={isMenuOpen} onOpenChange={setIsMenuOpen}>
+                    <SheetTrigger asChild>
+                        <Button
+                            variant="outline"
+                            size="icon"
+                            className="md:hidden"
                         >
-                            <LogoIcon className="h-full w-28 mb-12" />
-                            <span className="sr-only">BaliBlissed</span>
-                        </Link>
-                        <Link
-                            href="/"
-                            className="text-muted-foreground hover:text-foreground"
-                            prefetch={false}
-                            onClick={() => setIsMenuOpen(false)}
-                        >
-                            Home
-                        </Link>
-                        <Link
-                            href="/private-car-charter"
-                            className="text-muted-foreground hover:text-foreground"
-                            prefetch={false}
-                            onClick={() => setIsMenuOpen(false)}
-                        >
-                            Car Charter
-                        </Link>
-                        <Link
-                            href="/#destinations"
-                            className="text-muted-foreground hover:text-foreground"
-                            prefetch={false}
-                            onClick={() => setIsMenuOpen(false)}
-                        >
-                            Destinations
-                        </Link>
-                        <Link
-                            href="/#packages"
-                            className="text-muted-foreground hover:text-foreground"
-                            prefetch={false}
-                            onClick={() => setIsMenuOpen(false)}
-                        >
-                            Packages
-                        </Link>
-                        <Link
-                            href="/#contact"
-                            className="text-muted-foreground hover:text-foreground"
-                            prefetch={false}
-                            onClick={() => setIsMenuOpen(false)}
-                        >
-                            Contact
-                        </Link>
-                        <div className="absolute bottom-4 left-4">
-                            <ThemeSwitcher />
-                        </div>
-                    </nav>
-                </SheetContent>
-            </Sheet>
+                            <Menu className="h-6 w-6" />
+                            <span className="sr-only">Toggle navigation menu</span>
+                        </Button>
+                    </SheetTrigger>
+                    <SheetContent side="right">
+                        <SheetHeader className="sr-only">
+                            <SheetTitle>Mobile Menu</SheetTitle>
+                            <SheetDescription>
+                                Navigation links for the BaliBlissed website.
+                            </SheetDescription>
+                        </SheetHeader>
+                        <nav className="grid max-w-4 gap-8 text-lg font-medium mt-4">
+                            <Link
+                                href="/"
+                                className="flex items-center gap-2 text-lg font-semibold"
+                                prefetch={false}
+                                onClick={() => setIsMenuOpen(false)}
+                            >
+                                <LogoIcon className="h-full w-28 mb-12" />
+                                <span className="sr-only">BaliBlissed</span>
+                            </Link>
+                            <Link
+                                href="/"
+                                className="text-muted-foreground hover:text-foreground"
+                                prefetch={false}
+                                onClick={() => setIsMenuOpen(false)}
+                            >
+                                Home
+                            </Link>
+                            <Link
+                                href="/private-car-charter"
+                                className="text-muted-foreground hover:text-foreground"
+                                prefetch={false}
+                                onClick={() => setIsMenuOpen(false)}
+                            >
+                                Car Charter
+                            </Link>
+                            <Link
+                                href="/#destinations"
+                                className="text-muted-foreground hover:text-foreground"
+                                prefetch={false}
+                                onClick={() => setIsMenuOpen(false)}
+                            >
+                                Destinations
+                            </Link>
+                            <Link
+                                href="/#packages"
+                                className="text-muted-foreground hover:text-foreground"
+                                prefetch={false}
+                                onClick={() => setIsMenuOpen(false)}
+                            >
+                                Packages
+                            </Link>
+                            <Link
+                                href="/#contact"
+                                className="text-muted-foreground hover:text-foreground"
+                                prefetch={false}
+                                onClick={() => setIsMenuOpen(false)}
+                            >
+                                Contact
+                            </Link>
+                        </nav>
+                    </SheetContent>
+                </Sheet>
+            </div>
         </header>
     );
 }
