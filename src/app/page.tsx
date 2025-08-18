bb471c405829bd6104e088e06b4c9d8928c04691
Made Suryawan <suryawankusuma@gmail.com>
2025-08-18 10:37:46 +0000

    i will try to run npm build

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -71,10 +71,17 @@ import {
     AlertDialogTitle,
 } from "@/components/ui/alert-dialog";
 import { type DateRange } from "react-day-picker";
-import { TempleIcon } from "@/components/icons/TempleIcon";
-import { DanceIcon } from "@/components/icons/DanceIcon";
 import { useToast } from "@/hooks/use-toast";
 import { Label } from "@/components/ui/label";
+
+const TempleIcon = (props: React.SVGProps<SVGSVGElement>) => (
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" {...props}><path d="M11.99 2.25L2.69 9.33l-1.39 1.58v1.8l4.6-1.58v6.75h1.5v-4.5l1.88-.63v5.13h1.5v-4.5l1.88-.63v5.13h1.5v-4.5l1.88-.63v5.13h1.5V11.13l4.6 1.58v-1.8l-1.39-1.58L11.99 2.25zM9.25 11.25H14.75"></path></svg>
+)
+
+const DanceIcon = (props: React.SVGProps<SVGSVGElement>) => (
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" {...props}><path d="M12 4a4 4 0 1 0 4 4 4 4 0 0 0-4-4zm0 14.5a.5.5 0 0 1-.5-.5v-6a.5.5 0 0 1 1 0v6a.5.5 0 0 1-.5.5z"/><path d="M6.5 11.5a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 1 0v1a.5.5 0 0 1-.5.5zM17.5 11.5a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 1 0v1a.5.5 0 0 1-.5.5z"/><path d="M12 14.5a6 6 0 0 0-6-6H4.5a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5H6"/><path d="M12 14.5a6 6 0 0 1 6-6h1.5a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5H18"/></svg>
+)
+
 
 const HeroSection = (): React.JSX.Element => (
     <section className="relative w-full h-[80vh] min-h-[480px]">
@@ -932,3 +939,5 @@ export default function Home(): React.JSX.Element {
         </>
     );
 }
+
+    
